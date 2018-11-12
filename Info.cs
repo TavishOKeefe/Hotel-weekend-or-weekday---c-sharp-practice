3c19b7ecdbb948575910523f4889c3e9aa211233
Padma Patil <padma@email.com>
2018-11-11 17:14:58 -0800

    create questions for hotel stay

---
+++ Info.cs
@@ -0,0 +1,25 @@
+using System;
+
+class HotelCost
+{
+  static void Main()
+  {
+    Console.WriteLine("In what season are you booking the hotel?");
+    string season = Console.ReadLine();
+
+    Console.WriteLine("On the weekend or a weeknight?");
+    string dayOfWeek = Console.ReadLine();
+
+    bool summer = season == "summer";
+    bool weekend = dayOfWeek == "weekend";
+
+    if (summer && weekend)
+    {
+      Console.WriteLine("Your stay is probalby going to expensive. Your stay is both peak season and the weekend.");
+    }
+    else
+    {
+      Console.WriteLine("Your stay might be expensive, but it could be worse!");
+    }
+  }
+}
